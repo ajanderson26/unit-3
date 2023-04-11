405e7370234c221c330decee211297b8d21d8fc5
ajanderson26 <117036528+ajanderson26@users.noreply.github.com>
2023-04-11 15:02:36 -0500

    activity 9

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,168 +1,87 @@
-//execute script when window is loaded
-window.onload = function () {
+//begin script when window loads
+window.onload = setMap();
 
-    var container = d3.select("body") //get the <body> element from the DOM
-        .append("svg")  //put new svg in the body
-        .attr("width", 950)
-        .attr("height", 500)
-        .attr("class", "container") //assign a class name
-        .style("background-color", "rgba(235, 94, 40)") //svg background color
+//set up choropleth map
+function setMap(){
 
-    //innerRect block
-    var innerRect = container.append("rect")
-    .datum(400) //a single value is a DATUM
-    .attr("width", function(d){ //rectangle width
-        return d * 2; //400 * 2 = 800
-    })
-    .attr("height", function(d){ //rectangle height
-        return d; //400
-    })
-    .attr("class", "innerRect") //class name
-    .attr("x", 50) //position from left on the x (horizontal) axis
-    .attr("y", 50) //position from top on the y (vertical) axis
-    .style("fill", "#FFFFFF"); //fill color
+    function callback(data){
+        csvData = data[0];    
+        blockGroup = data[1];    
+        //france = data[2];
+        console.log(csvData);
+        console.log(blockGroup);
 
-  
-    var cityPop = [
-        { 
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        }
-    ];
+        var blockGroups = topojson.feature(blockGroup, blockGroup.objects.blockGroups).features;
+            //franceRegions = topojson.feature(france, france.objects.FranceRegions);
 
-    var x = d3.scaleLinear() //create the scale
-    .range([90, 750]) //output min and max
-    .domain([0, 3]); //input min and max
+            //create graticule generator
+            var graticule = d3.geoGraticule()
+            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+               
+                //create graticule background
+                var gratBackground = map.append("path")
+                .datum(graticule.outline()) //bind graticule background
+                .attr("class", "gratBackground") //assign class for styling
+                .attr("d", path) //project graticule
+    
+            //create graticule lines
+               //var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+               //.data(graticule.lines()) //bind graticule lines to each element to be created
+               //.enter() //create an element for each datum
+              // .append("path") //append each element to the svg as a path element
+              // .attr("class", "gratLines") //assign class for styling
+              // .attr("d", path); //project graticule lines
+    
+        //examine the results
+       // console.log(europeCountries);
+        //add Europe countries to map
+        var county = map.append("path")
+            .datum(blockGroups)
+            .attr("class", "county")
+            .attr("d", path);
 
-//find the minimum value of the array
-var minPop = d3.min(cityPop, function(d){
-    return d.population;
-});
+        //add France regions to map --> do I need to have another enumeration unit?
+        var regions = map
+            .selectAll(".regions")
+            .data(blockGroups)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                //what do I put for admin1_code? --> unique identifier in the data
+                return "regions " + d.properties.adm1_code;
+            })
+            .attr("d", path);
 
-//find the maximum value of the array
-var maxPop = d3.max(cityPop, function(d){
-    return d.population;
-});
-
-//scale for circles center y coordinate
-var y = d3.scaleLinear()
-    .range([450, 50]) //was 440, 95
-    .domain([0, 700000]); //was minPop, maxPop
-
-
-    //color scale generator 
-    var color = d3.scaleLinear()
-        .range([
-            "#FDBE85",
-            "#D94701"
-        ])
-        .domain([
-            minPop, 
-            maxPop
-        ]);
-
- 
-    var circles = container.selectAll(".circles") //create an empty selection
-        .data(cityPop) //here we feed in an array
-        .enter() //one of the great mysteries of the universe
-        .append("circle") //inspect the HTML--holy crap, there's some circles there
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            //calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            //use the scale generator with the index to place each circle horizontally
-            return x(i);
-        })
-       //put in syntax above to apply the scale and this is what was there before scale was put in place .attr("cx", function(d, i){
-            //use the index to place each circle horizontally
-            //return 90 + (i * 180);
-        //})
         
-        .attr("cy", function(d){
-            return y(d.population);
-        })
+    }
 
-        .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000"); //black circle stroke
 
-        //below is the old cy function to calculate the y values?
-        //.attr("cy", function(d){
-            //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
-           // return 450 - (d.population * 0.0005);
-       // });\
+    //map frame dimensions
+    var width = 960,
+        height = 460;
 
-      // create y axis generator
-    var yAxis = d3.axisLeft(y);
+    //create new svg container for the map
+   var map = d3.select("body")
+       .append("svg")
+       .attr("class", "map")
+       .attr("width", width)
+       .attr("height", height);
 
-    //create axis g element and add axis
-    var axis = container.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(50, 0)")
-    .call(yAxis)
+    //create Albers equal area conic projection centered on Dane County
+    var projection = d3.geoConicEqualArea()
+    .center([-5.5, 44.8])
+    .rotate([81, 0, 0.5])
+    .parallels([26, 25])
+    .scale(15000)
+    .translate([width / 2, height / 2])
+    .angle([4]);
 
-        //create a text element and add the title
-        var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", 450)
-        .attr("y", 30)
-        .text("City Populations");
+        var path = d3.geoPath()
+        .projection(projection);
 
-        //create circle labels
-        var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "center")
-        .attr("y", function(d){
-            //vertical position centered on each circle
-            return y(d.population);
-
-        });
-
-    //first line of label
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .text(function(d){
-            return d.city;
-        });
-
-     //create format generator
-     var format = d3.format(",");
-
-     //second line of label
-     var popLine = labels.append("tspan")
-         .attr("class", "popLine")
-         .attr("x", function(d,i){
-             return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-         })
-         .attr("dy", "15") //vertical offset
-         .text(function(d){
-             return "Pop. " + format(d.population); //use format generator to format numbers
-         });
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [];    
+    promises.push(d3.csv("data/allOccur.csv")); //load attributes from csv    
+    promises.push(d3.json("data/block-groups.json")); //load background spatial data       
+    Promise.all(promises).then(callback);
 }
